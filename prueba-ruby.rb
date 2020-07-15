2a196ea2215ab79d822ccb142525ef3f3f9d438d
Tamara Rubilar <ta.rubilar@gmail.com>
2020-07-15 02:41:59 -0400

    the creation of the program begins

---
+++ prueba-ruby.rb
@@ -0,0 +1,32 @@
+=begin 
+
+Prueba - Introducción a la programacióncon Ruby
+
+En este desafío nos conectaremos a la API de la NASA para descargar fotos de los Rovers y filtar
+resultados acorde a lo pedido.
+
+Se pide:
+
+1. Crear el método request que reciba una url y el api_key y devuelva el hash con los resultados
+    Concatenar la API Key en la url
+
+2. Crear un método llamado buid_web_page que reciba el hash de respuesta con todos los datos
+    y construya una página web. Se evaluará la página creada y tiene que tener este formato:
+
+<html>
+<head>
+</head>
+<body>
+<ul>
+    <li><img src='.../398380645PRCLF0030000CCAM04010L1.PNG'></li>
+    <li><img src='.../398381687EDR_F0030000CCAM05010M_.JPG'></li>
+</ul>
+</body>
+</html>
+
+Se revisará la estructura del documento generado.
+
+3. Pregunta bonus: Crear un método photos_count que reciba el hash de respuesta y devuelva
+    un nuevo hash con el nombre de la camara y la cantidad de fotos.
+
+=end
